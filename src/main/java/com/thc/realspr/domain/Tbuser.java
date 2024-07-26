91784631917fdc3294bf9e6c6aad3f9279b2a51f
YearaChoi <22200750@handong.ac.kr>
2024-07-26 17:29:09 +0900

    feat: java 17로 변경, DB 연결 변경

--- src/main/java/com/thc/realspr/domain/Tbuser.java
+++ src/main/java/com/thc/realspr/domain/Tbuser.java
@@ -1,6 +1,6 @@
 package com.thc.realspr.domain;
 
-import jakarta.persistence.*;
+import javax.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
